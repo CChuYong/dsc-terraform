877132393c0a39c2caa228a95f6d020f2748cd58
ChuYong <jakgon@naver.com>
2023-06-06 20:00:12 +0900

    feat: 상태 s3관리

--- provider.tf
+++ provider.tf
@@ -4,9 +4,19 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+  backend "s3" {
+    bucket         = "bsc-tf-state"
+    key            = "terraform/dsc/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
 }
 
 provider "aws" {
-  region = "ap-northeast-2" # Seoul Region
+  region = "ap-northeast-2"
 }
 
+module "prod" {
+  source = "./production"
+}
