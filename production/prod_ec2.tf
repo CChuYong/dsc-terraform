dbed0bb85108f11915cc0339a1caed9681e02284
ChuYong <jakgon@naver.com>
2023-06-26 16:06:30 +0900

    feat: VPN용 bastion 호스트 등록

---
+++ production/prod_ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "vpn_bastion" {
+  ami           = "ami-08216dbbe630e9894"
+  instance_type = "t4g.small"
+  subnet_id = module.dsc_prod_vpc.public_subnet_ids[0]
+  associate_public_ip_address = true
+  security_groups = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "bastion-host"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_sg"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = module.dsc_prod_vpc.vpc_id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
